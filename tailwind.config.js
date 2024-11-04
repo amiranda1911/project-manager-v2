ed0ef497a62e64512e4ba543f7a34d6ccfa8afac
Anderson Miranda <amiranda@amiranda.dev.br>
2024-11-04 19:50:46 -0300

    feat: added font sizes to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ export default {
       fontFamily: {
         sans: ['Roboto', 'sans-serif'], // Define 'Roboto' como a fonte padrão para 'sans'
       },
+      fontSize: {
+        'xs': '0.75rem',  // 12px
+        'sm': '0.875rem', // 14px
+        'base': '1rem',   // 16px
+        'lg': '1.75rem',  // 28px
+        'xl': '2.5rem',   // 40px
+      },
     },
   },
   plugins: [],
