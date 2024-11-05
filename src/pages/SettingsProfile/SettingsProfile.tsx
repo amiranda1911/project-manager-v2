055d04c7d2fc0111b529ce1816cad5f025b797e6
Sabrina Souza <sabrinasouza.dev@gmail.com>
2024-11-05 17:16:31 -0300

    Responsive adjustments on settings page: improved layout for mobile and larger screens

--- src/pages/SettingsProfile/SettingsProfile.tsx
+++ src/pages/SettingsProfile/SettingsProfile.tsx
@@ -3,34 +3,34 @@ export const SettingsProfile = () => {
     const img = `https://images.unsplash.com/photo-1728887823143-d92d2ebbb53a?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`
 
   return (
-    <div className="p-24 leading-9">
+    <div className="p-5 sm:leading-8 sm:text-sm md:leading-9 md:text-base lg:p-24">
         <div className="my-5">
-            <div className="flex items-center mb-9">
-                <picture className="flex w-44 rounded mr-8">
-                    <img src={img} alt="Imagem de perfil" className="w-full h-full rounded-full shadow-md" /> {/*shadow arrumar*/}
+            <div className="flex items-center mb-9 w-full">
+                <picture className="flex mr-4 md:mr-8">
+                    <img src={img} alt="Imagem de perfil" className="w-29 h-30 min-w-29 rounded-full shadow-md md:w-45 md:h-46" /> {/*shadow arrumar*/}
                 </picture>
                 <div>
-                    <h1 className="text-5xl font-medium">John Doe</h1>
-                    <p className="text-19 opacity-70">john.doe@example.com</p>
+                    <h1 className="text-21 font-medium md:text-5xl">John Doe</h1>
+                    <p className="text-xl opacity-70 md:text-19">john.doe@example.com</p>
                 </div>
             </div>
             <div>
                 <h2 className="text-20 font-medium">Project profile</h2>
-                <p className="opacity-50">Update your profile information in the sections below.</p>
+                <p className="text-black-opacity-50">Update your profile information in the sections below.</p>
             </div>
         </div>
-        <hr />
-        <div className="flex justify-between">
+        <hr className="my-5 lg:my-0"/>
+        <div className="flex flex-col  lg:flex-row">
             <div className="w-1/4 min-w-96 mt-2">
                 <h2 className="text-2xl font-medium">Basic information</h2>
-                <p>Update your name and e-mail in this section. Note: this information will be public to all your project colleagues and can be changed anytime.</p>
+                <p className="text-black-opacity-50">Update your name and e-mail in this section. Note: this information will be public to all your project colleagues and can be changed anytime.</p>
             </div>
-            <form action="" method="post" className="flex w-3/6 flex-wrap mx-11 mt-6 mb-10 leading-5 text-sm">
-                <div className="flex flex-col w-1/2">
+            <form action="" method="post" className="flex flex-col flex-wrap  mt-6 mb-10 leading-5 text-sm md:w-10/12 md:flex-row lg:mx-11">
+                <div className="flex flex-col w-full md:w-1/2">
                     <label htmlFor="update-first-name" className="font-medium my-1.5">First name</label>
-                    <input type="text" id="update-first-name" className="p-2 border border-gray-300 rounded-md mr-5" placeholder="New first name"/>
+                    <input type="text" id="update-first-name" className="p-2 border border-gray-300 rounded-md md:mr-5" placeholder="New first name"/>
                 </div>
-                <div className="flex flex-col w-1/2">
+                <div className="flex flex-col w-full md:w-1/2">
                     <label htmlFor="update-last-name" className="font-medium my-1.5">Last name</label>
                     <input type="text" id="update-last-name" className="p-2 border border-gray-300 rounded-md" placeholder="New last name"/>
                 </div>
@@ -41,18 +41,49 @@ export const SettingsProfile = () => {
             </form>
         </div>
         <hr/>
-        <div className="flex justify-between">
+        <div className="flex flex-col justify-between lg:flex-row">
             <div className="w-1/4 min-w-96">
                 <h2 className="text-2xl font-medium">Profile picture</h2>
-                <p>Update your profile picture. Supported files are JPG, PNG, WebP and JPEG.</p>
+                <p className="text-black-opacity-50">Update your profile picture. Supported files are JPG, PNG, WebP and JPEG.</p>
             </div>
-            <div  className="flex w-3/6 flex-wrap mx-11 my-6 leading-5 text-sm">
-                <picture className="flex w-44 rounded mr-8">
+            <div  className="flex flex-col w-3/6 my-6 md:flex-row md:mx-11">
+                <picture className="flex w-45 h-46 min-w-44 min-h-44 shadow-md rounded-full mr-8 items-center justify-center">
                     <img src={img} alt="Imagem de perfil" className="w-full h-full rounded-full shadow-md" /> {/*shadow arrumar*/}
                 </picture>
                 <div>
                     <p>Add new profile picture</p>
                     <input type="file" />
+                </div>
+            </div>
+        </div>
+        <hr />
+        <div className="flex flex-col justify-between lg:flex-row">
+            <div className="w-1/4 min-w-96">
+                <h2 className="text-2xl font-medium">Communication</h2>
+                <p className="text-black-opacity-50">Update your e-mail communication preferences anytime. Choose to receive project notifications or disable them completely.</p>
+            </div>
+            <div className="flex flex-wrap my-6 text-gray-850 text-12 lg:mx-11 lg:w-3/6">
+                <div>
+                    <label htmlFor="new-tasks" className="flex">
+                        <input type="radio" id="new-tasks" name="notifications-update" className="mr-2" />
+                        <p>New tasks</p>
+                    </label>
+                    <p>Receive an e-mail alert each time a new task is assigned to me in a project.</p>
+                </div>
+                <div>
+                    <label htmlFor="new-team-members" className="flex">
+                        <input type="radio" id="new-team-members" name="notifications-update" className="mr-2" /> 
+                        <p>New team members</p>
+                    </label>
+                    <p>Receive an e-mail alert each time a team members enters in a project I’m assigned to.</p>
+                </div>
+                
+                <div>
+                    <label htmlFor="weekly-reports" className="flex">
+                        <input type="radio" id="weekly-reports" name="notifications-update" className="mr-2" />
+                        <p>Weekly reports</p>
+                    </label>
+                    <p>Receive a weekly e-mail with a basic report, including estimated time and most active members.</p>
                 </div>
             </div>
         </div>
