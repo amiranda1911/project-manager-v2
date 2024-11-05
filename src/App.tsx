9db28d47766cf2931a00a08a7c7ab3016de455d0
AdsonTanajura <adson.tanajura@gmail.com>
2024-11-04 21:41:08 -0300

    feat: add profile-page-mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import {Routes, Route, BrowserRouter} from 'react-router-dom'
-import Home from './pages/Home/Home'
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import ProfilePage from './pages/ProfilePage/ProfilePage';
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
