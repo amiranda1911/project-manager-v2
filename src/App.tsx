6eb141d3d609d8905288e64f432a64c7ec104cdf
AdsonTanajura <adson.tanajura@gmail.com>
2024-11-05 15:36:50 -0300

    congit to push

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import ProfilePage from './pages/ProfilePage/ProfilePage';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ProfilePage />} />
       </Routes>
     </BrowserRouter>
   );
