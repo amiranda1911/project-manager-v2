10d89c10f5883f5c153c280aaa28cc70bd4bd7b4
Anderson Miranda <amiranda@amiranda.dev.br>
2024-11-05 21:48:20 -0300

    feat: add responsive styles for md and lg screens

--- src/pages/pages/Kanban.tsx
+++ src/pages/pages/Kanban.tsx
@@ -1,11 +1,13 @@
 import KanbanColumn from '../../Components/KanbanColumn'
-import kanbanCanvasBgImage from '../../../public/kanban-bg.png' 
+import kanbanCanvasBgImage from '/public/kanban-bg.png' 
 const Kanban = () => {
   return (
     <div className='flex flex-grow'>
       <section id='kanban-canvas' className="
           flex flex-grow flex-row
-          w-full m-2 
+          w-full
+          md:w-[54.25rem] md:h-[44rem] 
+          m-2 
           items-center
           justify-center
           rounded-[1.875rem]
