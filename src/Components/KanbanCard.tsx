edd87bf3a75a467c86f54a83fec3d6395603c2bb
Anderson Miranda <amiranda@amiranda.dev.br>
2024-11-06 13:59:31 -0300

    fix: lg and mb styles fixes

--- src/Components/KanbanCard.tsx
+++ src/Components/KanbanCard.tsx
@@ -10,31 +10,32 @@ interface KanbanCardProps{
 const KanBanCard = ({priority} : KanbanCardProps) => {
   return (
     <div className="
-        flex flex-col
-		w-[6.116rem] h-[4.004rem] m-0 p-[0.26375rem]
+        flex flex-col box-border justify-between
+		w-[6.116rem] h-[4.004rem] m-0 p-[0.95rem]
 		md:w-[11.5rem] md:h-[7.53rem]
 		lg:w-[17.38rem] lg:h-[11.38rem]
-		justify-between
 		bg-white
 		rounded-[0.528125rem]
 		mx-auto my-2
+		
 		">
 			<span className={`
 				flex
 				font-jakarta 
-				text-1 md:text-6
+				text-1 md:text-6 lg:text-12
 				px-[0.17625rem] py-[0.088125rem]
-				w-fit justify-start rounded-full
+				w-fit justify-start rounded-[1rem]
+				lg:p-2
 				card-priority-${priority.toLowerCase()}
 			`}>
 				{priority}
 			</span>
 			<h2 className=" font-roboto font-bold 
-				text-3 md:text-10"
+				text-3 md:text-10 lg:text-16"
 			>
 				Task 3</h2>
 			<div className=" w-full font-jakarta	
-					text-2 md:text-7
+					text-2 md:text-7 lg:text-14
 			 		flex flex-row	justify-between "
 			>
 				<span>Progress</span>
@@ -43,10 +44,9 @@ const KanBanCard = ({priority} : KanbanCardProps) => {
         
 			<ProgressBar progress={30}/>
 			<footer className="flex flex-row
-					h-[11px] 
+					lg:h-[2rem] 
 					overflow-hidden
-			"
-			>
+			">
 				<div className="w-1/2 relative flex">
 					{Array(5).map((_, index) => (
             <img key={index} className="flex w-[11px] h-[11px] bg-black rounded-full"/>
@@ -55,9 +55,9 @@ const KanBanCard = ({priority} : KanbanCardProps) => {
 				</div>
 				<div className="
 					w-1/2 
-					text-3 font-semibold font-roboto
-					flex justify-between"
-				>
+					text-3 lg:text-16 font-semibold font-roboto
+					flex justify-between
+					">
 					<span className="flex items-center"><AiFillMessage color="#94A3B8" />11</span>
 					<span className="flex items-center"><FaCheckCircle color="#94A3B8" />187</span>
 				</div>
