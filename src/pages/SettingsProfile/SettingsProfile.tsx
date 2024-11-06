3994d0e036e92214ef56cb2f7ed73dc20c6cd68d
Sabrina Souza <sabrinasouza.dev@gmail.com>
2024-11-06 13:34:31 -0300

    chore: make small code adjustments (settings page)

--- src/pages/SettingsProfile/SettingsProfile.tsx
+++ src/pages/SettingsProfile/SettingsProfile.tsx
@@ -1,10 +1,8 @@
-import './SettingsProfile.css'
-
 const SettingsProfile = () => {
   const img = `https://images.unsplash.com/photo-1728887823143-d92d2ebbb53a?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`;
 
   return (
-    <div className="p-5 leading-9 text-sm md:leading-9 md:text-base lg:p-24">
+    <div className="p-5 text-sm/9 md:text-base/9 lg:p-24">
       <div className="my-5">
         <div className="flex items-center mb-9 w-full">
           <picture className="flex mr-4 md:mr-8">
