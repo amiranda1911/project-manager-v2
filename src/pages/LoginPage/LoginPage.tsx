5ee82904a4fb80752ceef60856c4fd0d61cbde45
Thais Sanches <thaisres@live.com>
2024-11-05 21:54:07 -0300

    style:LoginPage to mobile

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,36 +1,53 @@
-
+import { FaFacebook } from "react-icons/fa6";
+import { FcGoogle } from "react-icons/fc";
 
 const LoginPage = () => {
   return (
     <>
     <div className="bg-cover bg-center h-screen" style={{ backgroundImage: `url('public/LoginImg.svg')`}}>
-    <div className="absolute inset-0 bg-white opacity-70"></div>
-        <div>
-            <h1>Login Information</h1>
-            <p>Enter your credentials</p>
-            <p>New here? Let´s take you to 
-                <span><a href="/LoginPage" className="text-[#4f46e5] underline font-roboto font-bold text-15"> Sign up.</a></span>
-            </p>
-        </div>
-            <form className="">  {/*onSubmit={handleSubmit} */}
-                    
-                    <div>
-                        <label htmlFor="email">Last Name</label>                  
-                        <input type="email" id="email" placeholder="Enter your email"/>
-                    </div>
-                    <div>
-                        <label htmlFor="Password">Last Name</label>                  
-                        <input type="paswword" id="Password" placeholder="Enter your password"/>
-                    </div>
-
-            </form>
-            <button type="submit" className=""> Create an account </button>
-            <h3>or sign up with...</h3>
-            <div>
-                <a href="/" className=""> fb</a>
-                <a href="/" className=""> google</a>
-            </div>
         
+        {/*Translucent background */}
+        <div className="absolute inset-0 bg-white opacity-70">
+        </div>
+        
+        <div className="flex items-center justify-center w-[22.063rem] h-[39rem] bg-white absolute inset-0 m-auto rounded-[1.875rem] md:hidden"> {/*White Square */}
+            <div className="relative z-10">
+
+                <section className="leading-[1.5] pb-[1.5rem]">
+                    <h1 className="font-roboto font-bold text-center text-19 text-[#331436] pb-[1.5rem]">Login Information</h1>
+                    <p className="font-roboto font-normal text-center text-15 text-[#331436]">Enter your credentials</p>
+                    <p className="font-roboto font-normal text-center text-15 text-[#331436]">New here? Let´s take you to 
+                        <span><a href="/LoginPage" className="text-[#4f46e5] font-roboto font-bold text-15"> Sign up.</a></span>
+                    </p>
+                </section>
+
+                <section className=" flex flex-col">
+                    <form className="flex flex-col gap-[1.5rem]">  {/*onSubmit={handleSubmit} */}                        
+                        <div className="flex flex-col w-[20.563rem] h-[3.75rem]">
+                            <label htmlFor="email" className="font-roboto font-medium text-left text-14 text-[#331436]">Last Name</label>                  
+                            <input type="email" id="email" placeholder="Enter your email" className="font-roboto font-normal text-14 text-[#00000080] p-[0.75rem]
+                            w-[20.563rem] h-[2.25rem] border border-[#0000001A] rounded-md focus:outline-none focus:ring-1 focus:ring-[#00000080]"/>
+                        </div>
+                        <div className="flex flex-col w-[20.563rem] h-[3.75rem]">
+                            <label htmlFor="Password" className="font-roboto font-medium text-left text-14 text-[#331436]">Last Name</label>                  
+                            <input type="paswword" id="Password" placeholder="Enter your password" className="font-roboto font-normal text-14 text-[#00000080] p-[0.75rem]
+                            w-[20.563rem] h-[2.25rem] border border-[#0000001A] rounded-md focus:outline-none focus:ring-1 focus:ring-[#00000080]"/>
+                        </div>
+                    </form>
+
+                    <button type="submit" className="w-[20.563rem] h-[2.25rem] bg-[#1e293b] rounded-md text-white font-roboto font-medium font-15 mt-[2.938rem] mb-[2.5rem]" > 
+                        Login 
+                    </button>
+
+                    <h3 className="font-roboto font-normal text-center text-14 text-[#331436] mb-[0.563rem]">or sign up with...</h3>
+                    
+                    <div className="flex flex-row items-center justify-center gap-[0.813rem]">
+                        <a href="/" className="flex items-center justify-center border rounded-full border-[#3333334D] w-[6.875rem] h-[3.5rem] text-[#0C82EE] text-23" > <FaFacebook /> </a>
+                        <a href="/" className="flex items-center justify-center border rounded-full border-[#3333334D] w-[6.875rem] h-[3.5rem] text-23"> <FcGoogle /> </a>
+                    </div>
+                </section>    
+            </div>
+        </div>    
     </div>
     
     </>
--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -14,31 +14,35 @@ const SignUpPage = () => {
   return (
     <>
     <main className="bg-cover bg-center h-screen" style={{ backgroundImage: `url('public/SignUpImg.svg')`}}>
-    <div className="absolute inset-0 bg-white opacity-70"></div>
-    <section className= "relative z-10">    
-        <div>
-            <h1>Sign up Information</h1>
-            <p>Already have an account? 
-                <span><a href="/LoginPage" className="text-[#4f46e5] underline font-roboto font-bold text-15"> Log in.</a></span>
-            </p>
-        </div>
-            <form className="">  {/*onSubmit={handleSubmit} */}
-                <section>
-                    <div>
-                        <label htmlFor="firstName">First Name</label>
-                        <input 
-                        type="text" 
-                        id="firstName" 
-                        placeholder="Enter your first name"
-                        //value={name}
-                        //onChange={(e) => setName(e.target.value)}
-                        />                                       
-                    </div>
-                    <div>
-                        <label htmlFor="lastName">Last Name</label>                  
-                        <input type="text" id="lastName" placeholder="Enter your last name"/>
-                    </div>
-                </section>
+    
+        {/*Translucent background */}
+        <div className="absolute inset-0 bg-white opacity-70"></div>
+        
+        <div className="flex items-center justify-center w-[22.063rem] h-[44.125rem] bg-white absolute inset-0 m-auto rounded-[1.875rem] md:hidden">{/*White Square */}
+            <section className= "relative z-10">    
+                <div>
+                    <h1>Sign up Information</h1>
+                    <p>Already have an account? 
+                        <span><a href="/LoginPage" className="text-[#4f46e5] underline font-roboto font-bold text-15"> Log in.</a></span>
+                    </p>
+                </div>
+                <form className="">  {/*onSubmit={handleSubmit} */}
+                    <section>
+                        <div>
+                            <label htmlFor="firstName">First Name</label>
+                            <input 
+                            type="text" 
+                            id="firstName" 
+                            placeholder="Enter your first name"
+                                //value={name}
+                                //onChange={(e) => setName(e.target.value)}
+                            />                                       
+                        </div>
+                        <div>
+                            <label htmlFor="lastName">Last Name</label>                  
+                            <input type="text" id="lastName" placeholder="Enter your last name"/>
+                        </div>
+                    </section>
 
                     <div>
                         <label htmlFor="email">Last Name</label>                  
@@ -53,15 +57,15 @@ const SignUpPage = () => {
                         <input type="paswword" id="Password" placeholder="Enter your password"/>
                     </div>
 
-            </form>
-            <button type="submit" className=""> Login </button>
-            <h3>or sign up with...</h3>
-            <div>
-                <a href="/" className=""> fb</a>
-                <a href="/" className=""> google</a>
-            </div>
-        </section>
-        
+                </form>
+                <button type="submit" className=""> Login </button>
+                <h3>or sign up with...</h3>
+                <div>
+                    <a href="/" className=""> fb</a>
+                    <a href="/" className=""> google</a>
+                </div>    
+            </section>
+        </div>
     </main>
     
     </>
