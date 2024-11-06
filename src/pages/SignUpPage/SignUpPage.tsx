3d085ef6dcc7bd5e875b76148065cdc82cbda065
Thais Sanches <thaisres@live.com>
2024-11-05 22:37:20 -0300

    fix:buttons in SignUpPage

--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -11,7 +11,6 @@ const SignUpPage = () => {
         console.log(`Name: ${name}`);
     }; */}
 
-
   return (
     <>
     <main className="bg-cover bg-center h-screen" style={{ backgroundImage: `url('public/SignUpImg.svg')`}}>
@@ -78,8 +77,8 @@ const SignUpPage = () => {
                     <h3 className="font-roboto font-normal text-center text-14 text-[#331436] mb-[0.563rem]">or sign up with...</h3>
 
                     <div className="flex flex-row items-center justify-center gap-[0.813rem]">
-                        <a href="/" className="flex items-center justify-center border rounded-full border-[#3333334D] w-[6.875rem] h-[3.5rem] text-[#0C82EE] text-23"> fb</a>
-                        <a href="/" className="flex items-center justify-center border rounded-full border-[#3333334D] w-[6.875rem] h-[3.5rem] text-23"> google</a>
+                        <a href="/" className="flex items-center justify-center border rounded-full border-[#3333334D] w-[6.875rem] h-[3.5rem] text-[#0C82EE] text-23"> <FaFacebook /> </a>
+                        <a href="/" className="flex items-center justify-center border rounded-full border-[#3333334D] w-[6.875rem] h-[3.5rem] text-23"> <FcGoogle /> </a>
                     </div>
 
                 </section>   
