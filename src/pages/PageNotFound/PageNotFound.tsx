d6a9ed16913d7785d772fc0f480ce4f9c11c1552
Thais Sanches <thaisres@live.com>
2024-11-07 16:27:10 -0300

    feat: added footer and header to pageNotFound

--- src/pages/PageNotFound/PageNotFound.tsx
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -1,29 +1,32 @@
-
+import MainHeader from "../../Components/MainHeader"
+import MainFooter from "../../Components/MainFooter"
 
 const PageNotFound = () => {
   return (
     <>      
-      <header className="h-[5rem] bg-blue-950"> </header>
+      <header>
+        <MainHeader pageType='page2'/>
+      </header>
 
-      <main className="flex flex-col h-screen items-center justify-center bg-white 
+      <main className="flex flex-col h-screen items-center bg-white 
             md:flex-row md:w-[43.75rem] md:mx-auto lg:w-[59.75rem]">
         
-        <div className="flex flex-col h-full justify-center gap-y-[3.75rem] flex-grow md:flex-row md:w-[43.938rem] md:mx-auto">
+        <div className="flex flex-col h-full pt-9 flex-grow md:flex-row md:w-[43.938rem] md:mx-auto">
             
             <div className="flex items-center justify-center md:place-content-start">
                 <img 
                 src="src\assets\PostIt404.svg" 
                 alt="404 Not Found" 
-                className="w-[12.68rem] md:w-[20.912rem] lg:w-[28.438rem]"
+                className="w-[12.68rem] md:w-auto lg:w-[28.438rem]"
                 />
             </div>                        
             
             <div className="flex flex-col items-center text-center w-[14.375rem] mx-auto md:place-content-center md:w-[20.22rem]">   
                 <h1 className="font-roboto font-normal text-26 md:text-[6rem] md:w-[20.22rem] md:text-left lg:w-[27.5rem]">404</h1>
-                <p className="font-roboto font-normal text-black text-[1.125rem] leading-[2] md:text-19 md:text-left lg:w-[27.5rem]">
+                <p className="font-roboto font-normal text-black-black text-[1.125rem] leading-[2] md:text-19 md:text-left lg:w-[27.5rem]">
                 Oops! Nothing to see here, just a lost kanban task.
                 </p>
-                <a href="/LoginPage" className="pt-3 text-[#4f46e5] underline font-roboto font-bold text-15 md:text-18 lg:w-[24.813rem] lg:text-right lg:pr-6" >
+                <a href="/login" className="pt-3 text-[#4f46e5] underline font-roboto font-bold text-15 md:text-18 lg:w-[24.813rem] lg:text-right lg:pr-6" >
                     Go back to home
                 </a>            
             </div>
@@ -31,7 +34,9 @@ const PageNotFound = () => {
         </div>
       </main>
 
-      <footer className="h-[12.875rem] bg-blue-900"> </footer>
+      <footer>
+        <MainFooter />
+      </footer>
     </>
   )
 }
