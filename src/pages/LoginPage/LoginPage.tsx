e700ae3f2abde8976984e8622675e01e93b831cf
Thais Sanches <thaisres@live.com>
2024-11-07 10:34:47 -0300

    feat:Created components to the Login and SignUp pages

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,7 @@
-import { FaFacebook } from "react-icons/fa6";
-import { FcGoogle } from "react-icons/fc";
 import MainHeader from "../../Components/MainHeader";
 import MainFooter from "../../Components/MainFooter";
+import FormLogin from "../../Components/FormLogin";
+import BottomButtons from "../../Components/BottomButtons";
 
 const LoginPage = () => {
   return (
@@ -41,31 +41,9 @@ const LoginPage = () => {
                     </section>
 
                 <section className="flex flex-col">
-                    <form className="flex flex-col gap-[1.5rem]">  {/*onSubmit={handleSubmit} */}                        
-                        <div className="flex flex-col w-[20.563rem] h-[3.75rem]">
-                            <label htmlFor="email" className="font-roboto font-medium text-left text-14 text-[#331436]">Last Name</label>                  
-                            <input type="email" id="email" placeholder="Enter your email" className="font-roboto font-normal text-14 text-[#00000080] p-[0.75rem]
-                            w-[20.563rem] h-[2.25rem] border border-[#0000001A] rounded-md focus:outline-none focus:ring-1 focus:ring-[#00000080]"/>
-                        </div>
-                        <div className="flex flex-col w-[20.563rem] h-[3.75rem]">
-                            <label htmlFor="Password" className="font-roboto font-medium text-left text-14 text-[#331436]">Last Name</label>                  
-                            <input type="paswword" id="Password" placeholder="Enter your password" className="font-roboto font-normal text-14 text-[#00000080] p-[0.75rem]
-                            w-[20.563rem] h-[2.25rem] border border-[#0000001A] rounded-md focus:outline-none focus:ring-1 focus:ring-[#00000080]"/>
-                        </div>
-                    </form>
+                    <FormLogin />
 
-                    <button type="submit" className="w-[20.563rem] h-[2.25rem] bg-[#1e293b] rounded-md text-white font-roboto font-medium font-15 mt-[2.938rem] mb-[2.5rem]
-                    cursor-pointer hover:shadow-lg hover:brightness-150 transition duration-200
-                    md:w-[31.75rem] md:h-[3rem] md:text-16 md:mt-[2rem] md:mb-[1.938rem]" > 
-                        Login 
-                    </button>
-
-                    <h3 className="font-roboto font-normal text-center text-14 text-[#331436] mb-[0.563rem]">or sign up with...</h3>
-                    
-                    <div className="flex flex-row items-center justify-center gap-[0.813rem]">
-                        <a href="/" className="flex items-center justify-center border rounded-full border-[#3333334D] w-[6.875rem] h-[3.5rem] text-[#0C82EE] text-23 cursor-pointer hover:shadow-lg hover:brightness-125" > <FaFacebook /> </a>
-                        <a href="/" className="flex items-center justify-center border rounded-full border-[#3333334D] w-[6.875rem] h-[3.5rem] text-23 cursor-pointer hover:shadow-lg hover:brightness-125"> <FcGoogle /> </a>
-                    </div>
+                    <BottomButtons />
                 </section>    
             </div>
         </div>    
--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,5 +1,5 @@
-import { FaFacebook } from "react-icons/fa6";
-import { FcGoogle } from "react-icons/fc";
+import FormSignUp from "../../Components/FormSignUp";
+import BottomButtons from "../../Components/BottomButtons";
 
 const SignUpPage = () => {
 
@@ -42,57 +42,9 @@ const SignUpPage = () => {
                     </section>
 
                     <section className="flex flex-col">
-                        <form className="flex flex-col gap-[1.5rem] md:mt-[1.938rem]">  {/*onSubmit={handleSubmit} */}
-                            
-                            <section className="md:flex md:flex-row md:gap-[1.125rem] md:max-w-[32.5rem]">
-                                <div className="flex flex-col w-[20.563rem] h-[3.75rem]">
-                                    <label htmlFor="firstName" className="font-roboto font-medium text-left text-14 text-[#331436]">First Name</label>
-                                    <input type="text" id="firstName" placeholder="Enter your first name" className="font-roboto font-normal text-14 text-[#00000080] p-[0.75rem] 
-                                    w-auto h-[2.25rem] border border-[#0000001A] rounded-md focus:outline-none focus:ring-1 focus:ring-[#00000080]"
-                                        //value={name}
-                                        //onChange={(e) => setName(e.target.value)}
-                                    />   
-                                </div>
+                        <FormSignUp />
 
-                                <div className="flex flex-col w-[20.563rem] h-[3.75rem]">
-                                    <label htmlFor="lastName" className="font-roboto font-medium text-left text-14 text-[#331436]">Last Name</label>                  
-                                    <input type="text" id="lastName" placeholder="Enter your last name" className="font-roboto font-normal text-14 text-[#00000080] p-[0.75rem]
-                                    w-auto h-[2.25rem] border border-[#0000001A] rounded-md focus:outline-none focus:ring-1 focus:ring-[#00000080]"/>
-                                </div>
-                            </section>
-                            
-                            <div className="flex flex-col w-[20.563rem] h-[3.75rem]">
-                                <label htmlFor="email" className="font-roboto font-medium text-left text-14 text-[#331436]">Email</label>                  
-                                <input type="email" id="email" placeholder="Enter your email" className="font-roboto font-normal text-14 text-[#00000080] p-[0.75rem]
-                                w-auto h-[2.25rem] border border-[#0000001A] rounded-md focus:outline-none focus:ring-1 focus:ring-[#00000080] md:w-[32.5rem]"/>
-                            </div>
-
-                            <div className="flex flex-col w-[20.563rem] h-[3.75rem]">
-                                <label htmlFor="jobPosition" className="font-roboto font-medium text-left text-14 text-[#331436]">Job Position</label>                  
-                                <input type="text" id="jobPosition" placeholder="Enter your job position (example: Project Manager)" className="font-roboto font-normal text-14 text-[#00000080] p-[0.75rem]
-                                w-auto h-[2.25rem] truncate border border-[#0000001A] rounded-md focus:outline-none focus:ring-1 focus:ring-[#00000080] md:w-[32.5rem]"/>
-                            </div>
-
-                            <div className="flex flex-col w-[20.563rem] h-[3.75rem]">
-                                <label htmlFor="Password" className="font-roboto font-medium text-left text-14 text-[#331436]">Password</label>                  
-                                <input type="paswword" id="Password" placeholder="Enter your password" className="font-roboto font-normal text-14 text-[#00000080] p-[0.75rem]
-                                w-auto h-[2.25rem] border border-[#0000001A] rounded-md focus:outline-none focus:ring-1 focus:ring-[#00000080] md:w-[32.5rem]"/>
-                            </div>
-
-                        </form>
-
-                        <button type="submit" className="mx-auto w-[20.563rem] h-[2.25rem] bg-[#1e293b] rounded-md text-white font-roboto font-medium text-15 mt-[1.413rem] mb-[1.313rem] 
-                        cursor-pointer hover:shadow-lg hover:brightness-150 transition duration-200
-                        md:w-[31.75rem] md:h-[3rem] md:text-16 md:mt-[2rem] md:mb-[1.938rem]"> 
-                            Create an account 
-                        </button>
-
-                        <h3 className="font-roboto font-normal text-center text-14 text-[#331436] mb-[0.563rem] md:text-16">or sign up with...</h3>
-
-                        <div className="flex flex-row items-center justify-center gap-[0.813rem]">
-                            <a href="/" className="flex items-center justify-center border rounded-full border-[#3333334D] w-[6.875rem] h-[3.5rem] text-[#0C82EE] text-23 cursor-pointer hover:shadow-lg hover:brightness-125"> <FaFacebook /> </a>
-                            <a href="/" className="flex items-center justify-center border rounded-full border-[#3333334D] w-[6.875rem] h-[3.5rem] text-23 cursor-pointer hover:shadow-lg hover:brightness-125"> <FcGoogle /> </a>
-                        </div>
+                        <BottomButtons />
 
                     </section>   
                 </div>
