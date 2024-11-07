6a1677b4cc63ea417d1760a6d980612999d16f50
Sabrina Souza <sabrinasouza.dev@gmail.com>
2024-11-07 12:26:35 -0300

    refactor: moved the form out of the main page and into its own component

--- src/pages/SettingsProfile/SettingsProfile.tsx
+++ src/pages/SettingsProfile/SettingsProfile.tsx
@@ -3,7 +3,7 @@ import { UploadImage } from "../../Components/UploadImage";
 
 const SettingsProfile = () => {
   const img = `https://images.unsplash.com/photo-1728887823143-d92d2ebbb53a?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`;
-
+  
   return (
     <div className="p-5 text-sm/9 md:text-base/9 lg:p-24 font-roboto">
       <div className="my-5">
