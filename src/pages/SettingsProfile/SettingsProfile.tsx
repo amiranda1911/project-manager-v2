19188fac32caa5d682021445e4bf2b59369dd222
Sabrina Souza <sabrinasouza.dev@gmail.com>
2024-11-08 20:05:39 -0300

    refactor: Add GET and PUT requests with pull functionality and new helper functions

--- src/pages/SettingsProfile/SettingsProfile.tsx
+++ src/pages/SettingsProfile/SettingsProfile.tsx
@@ -1,12 +1,63 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import { EnableNotifications } from "../../Components/EnableNotifications";
 import MainFooter from "../../Components/MainFooter";
 import MainHeader from "../../Components/MainHeader";
-import { UpdateInfos } from "../../Components/UpdateInfos";
 import { UpdateSocialnformation } from "../../Components/UpdateSocialnformation";
 import { UploadImage } from "../../Components/UploadImage";
+import { UpdateInfos } from "../../Components/UpdateInfos";
+
+import { UserData } from "./type";
 
 const SettingsProfile = () => {
   const img = `https://images.unsplash.com/photo-1728887823143-d92d2ebbb53a?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`;
+  
+  const [data, setData] = useState(useState<UserData | null>(null));
+  
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/users/0768");
+        setData(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  const updateInfosSettings = ( section:  keyof UserData | null, key: string, value: string | number | boolean): void => {
+    setData((prevState) => {
+      if (section) {
+        return {
+          ...prevState,
+          [section]: {
+            ...prevState![section as keyof UserData], // Mantém as outras chaves da seção
+            [key]: value, // Atualiza a chave específica
+          },
+        };
+      } else {
+        // Se não for passada uma seção, atualiza diretamente o valor da chave
+        return {
+          ...prevState,
+          [key]: value,
+        };
+      }
+    });
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.put("http://localhost:3000/users/0768", {data,});
+      console.log("Dados atualizados com sucesso:", response.data);
+    } catch (error) {
+      console.error("Erro ao atualizar dados:", error);
+    }
+  };
+
+  console.log(data);
 
   return (
     <div className=" text-sm/9 md:text-base/9  font-roboto">
@@ -48,7 +99,7 @@ const SettingsProfile = () => {
             </p>
           </div>
           {/*Update name, last name, email*/}
-          <UpdateInfos />
+          <UpdateInfos updateInfosSettings={updateInfosSettings} />
         </div>
         <hr className="my-4 lg:my-0" />
         <div className="flex flex-col justify-between lg:flex-row">
@@ -71,7 +122,7 @@ const SettingsProfile = () => {
               receive project notifications or disable them completely.
             </p>
           </div>
-          <EnableNotifications />
+          <EnableNotifications updateInfosSettings={updateInfosSettings} />
         </div>
         <hr className="my-4 lg:my-0" />
         <div className="flex-col lg:flex-row flex lg:justify-between">
@@ -82,16 +133,22 @@ const SettingsProfile = () => {
               receive project notifications or disable them completely.
             </p>
           </div>
-          <UpdateSocialnformation />
+          <UpdateSocialnformation updateInfosSettings={updateInfosSettings} />
         </div>
         <div className="flex justify-center items-center mt-8 lg:mt-20">
-          <button className="w-90 md:w-100 bg-primary-color-dark text-white rounded-lg h-12 ">
+          <button
+            className="w-90 md:w-100 bg-primary-color-dark text-white rounded-lg h-12 "
+            onClick={handleSubmit}
+          >
             Update information
           </button>
         </div>
         <div className="text-center my-4">
           <p className="text-sm ">
-            Never mind, take me <a href="/kanban" className="text-indigo-600 underline">back to my project.</a>
+            Never mind, take me{" "}
+            <a href="/kanban" className="text-indigo-600 underline">
+              back to my project.
+            </a>
           </p>
         </div>
       </main>
