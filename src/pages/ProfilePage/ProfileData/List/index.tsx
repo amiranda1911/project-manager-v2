5a04cb12bbdd7b0dd7ae80282f0789c1918f4bba
AdsonTanajura <adson.tanajura@gmail.com>
2024-11-08 19:44:59 -0300

    Fix: fix the responsiveness on the profile page

--- src/pages/ProfilePage/ProfileData/List/index.tsx
+++ src/pages/ProfilePage/ProfileData/List/index.tsx
@@ -23,9 +23,9 @@ const ProfileDataList = ({
   const { instagram, linkedin, twitter } = socialMedia;
   return (
     <>
-      <div className="flex gap-4 flex-col mt-2 lg:max-w-96">
-        <h3 className="font-medium text-2xl">Contact</h3>
-        <ul className="flex justify-center content-center flex-col gap-2">
+      <div className="mt-2 lg:max-w-96">
+        <h3 className="font-medium text-2xl ">Contact</h3>
+        <ul className="flex flex-col gap-2 mt-4">
           <ProfileDataListItem input={email} label="E-mail" />
           <ProfileDataListItem input={twitter} label="Twitter/x" />
           <ProfileDataListItem input={instagram} label="Instagram" />
@@ -33,9 +33,9 @@ const ProfileDataList = ({
         </ul>
       </div>
 
-      <div className="mt-[2.75rem] gap-4 border-b-2 pb-5 lg:border-b-0 lg:max-w-96">
+      <div className="mt-[2.75rem] border-b-2 pb-5 lg:border-b-0 lg:max-w-96">
         <h3 className="font-medium text-2xl">Details</h3>
-        <ul className="flex flex-col gap-2">
+        <ul className="flex flex-col gap-2 mt-4">
           <ProfileDataListItem label="User ID" input={id} />
           <ProfileDataListItem
             label="Creation date"
