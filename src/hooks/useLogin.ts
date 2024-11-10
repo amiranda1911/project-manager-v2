ded51328528959a1802d36d0fa5426188375661b
AdsonTanajura <adson.tanajura@gmail.com>
2024-11-09 21:12:44 -0300

    Feat: creat login test

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -3,6 +3,7 @@ import { baseUrl } from '../constants';
 import { UserProps } from '../interface/User';
 import { useAuth } from './useAuth';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface validateCredentials {
   email: string;
@@ -11,10 +12,12 @@ interface validateCredentials {
 
 export const useLogin = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const [isValidCredentials, setIsValidCredentials] = useState(false);
+  const [isValidCredentials, setIsValidCredentials] = useState<false | true>(); // Inicializando como false
   const [error, setError] = useState('');
 
   const { setToken } = useAuth();
+  const navigate = useNavigate();
+
   const validateCredentials = async ({
     email,
     password,
@@ -22,20 +25,22 @@ export const useLogin = () => {
     setIsLoading(true);
     try {
       const response = await axios.get(`${baseUrl}/users`);
+      const users: UserProps[] = response.data; // Corrigido para o tipo correto
+      const user = users.find(
+        (user) => user.email === email && user.password === password
+      );
 
-      const users: [UserProps] = response.data;
-      users.map((user) => {
-        if (user.email === email && user.password === password) {
-          setToken(user.id);
-          setIsValidCredentials(true);
-          return true;
-        } else {
-          setIsValidCredentials(false);
-        }
-      });
+      if (user) {
+        setToken(user.id);
+        setIsValidCredentials(true); // Usuário encontrado e credenciais válidas
+        navigate('/kanban'); // Redirecionando após credenciais válidas
+      } else {
+        setIsValidCredentials(false); // Usuário não encontrado ou credenciais inválidas
+      }
     } catch (error) {
-      setError('Ocorreu um erro desculpa pelo ocorrido!');
+      setError('Ocorreu um erro, desculpe pelo ocorrido!');
       console.log(error);
+      setIsValidCredentials(false); // Caso de erro na requisição
     } finally {
       setIsLoading(false);
     }
