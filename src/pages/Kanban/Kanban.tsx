294f7049ea4d4cc507e844ae784cc65c5280f90a
Anderson Miranda <amiranda@amiranda.dev.br>
2024-11-09 21:18:40 -0300

    fix: add kanban column new card handle

--- src/pages/Kanban/Kanban.tsx
+++ src/pages/Kanban/Kanban.tsx
@@ -1,88 +1,102 @@
+// Importa as bibliotecas e componentes necessários
 import { useEffect, useState } from 'react'
-import FixedMetrics from '../../Components/FixedMetrics'
-import KanbanColumn from '../../Components/KanbanColumn'
-import kanbanCanvasBgImage from '/public/kanban-bg.png' 
-import { BsPinFill } from 'react-icons/bs'
-import CreationModal from '../../Components/CreationModal'
-import axios from 'axios'
-import { Task } from '../../utils/Task'
-import { Status } from '../../utils/EnumStatus'
+import FixedMetrics from '../../Components/FixedMetrics' // Componente para métricas fixas no layout
+import KanbanColumn from '../../Components/KanbanColumn' // Componente que representa uma coluna no Kanban
+import kanbanCanvasBgImage from '/public/kanban-bg.png'  // Imagem de fundo para o Kanban
+import { BsPinFill } from 'react-icons/bs' // Ícone de pin para fixação de elementos na tela
+import CreationModal from '../../Components/CreationModal' // Componente modal para criação de tarefas
+import axios from 'axios' // Biblioteca para fazer requisições HTTP
+import { Task } from '../../utils/Task' // Definição do tipo de tarefa
+import { Status } from '../../utils/EnumStatus' // Enumeração para os status das tarefas
+
+// Componente principal do Kanban
 const Kanban = () => {
 
+  // Define o estado para controlar atualizações, visibilidade de métricas fixas e do modal de criação
   const [update, setUpdate] = useState<boolean>(true)
-  
   const [showFixedMetric, setShowFixedMetric] = useState<boolean>(false)
-  const [showCreationModal, setShowCreationModal] = useState<boolean>(true)
-  const [tasks, setTasks] = useState<Task[]>([])
+  const [showCreationModal, setShowCreationModal] = useState<boolean>(false)
+  const [tasks, setTasks] = useState<Task[]>([]) // Estado que armazena as tarefas
 
+  // Estados que armazenam as tarefas filtradas por status
   const [inProgressTasks, setInprogressTasks] = useState<Task[]>([])
   const [todoTasks, setTodoTasks] = useState<Task[]>([])
   const [doneTasks, setDoneTasks] = useState<Task[]>([])
 
+  // Hook useEffect para buscar tarefas do backend
   useEffect(() => {
     const fetchData = async () =>{
-      try{
-        const response = await axios.get('http://localhost:3000/tasks')
-        setTasks(response.data); 
-        console.log()
+      try {
+        const response = await axios.get('http://localhost:3000/tasks') // Faz requisição para buscar tarefas
+        setTasks(response.data);  // Atualiza o estado com as tarefas recebidas
       } catch (error) {
-        console.log(error)
+        console.log(error) // Exibe erro no console se a requisição falhar
       }
     }
 
-   fetchData()
-   setUpdate(true)
-  },[update])
+    fetchData()
+    setUpdate(true) // Marca a necessidade de uma nova atualização
+  }, [update]) // Executa sempre que `update` for alterado
 
+  // Hook useEffect para atualizar tarefas com base nos status
   useEffect(() => {
     const updateData = () => {
-      setDoneTasks(tasks.filter(task => task.status == Status.Done ))
-      setInprogressTasks(tasks.filter(task => task.status == Status.InProgress ))
-      setTodoTasks(tasks.filter(task => task.status == Status.ToDo ))
+      setDoneTasks(tasks.filter(task => task.status == Status.Done)) // Filtra tarefas concluídas
+      setInprogressTasks(tasks.filter(task => task.status == Status.InProgress)) // Filtra tarefas em progresso
+      setTodoTasks(tasks.filter(task => task.status == Status.ToDo)) // Filtra tarefas a serem feitas
     } 
     updateData()
-  },[tasks])
+  }, [tasks]) // Executa sempre que `tasks` for alterado
 
+  // Função para fechar o modal de criação de tarefa e atualizar o estado
   const handleCloseCreationModal = () => {
     setShowCreationModal(false)
-    setUpdate(false)
+    setUpdate(false) // Marca que uma atualização não é mais necessária
   }
-  
+
   return (
     <>
-    <div className={`flex flex-grow box-border w-full relative`}>
-      {showCreationModal && <CreationModal closeDispatch={handleCloseCreationModal}/>}
-      <div className={`
-        m-4 w-full
-        rounded-[1.875rem]
-        items-center
-        justify-center
-        flex flex-grow flex-row
-        ${showFixedMetric  ? 'lg:w-3/4': '' }
-      `}
-        style={{ backgroundImage: `url(${kanbanCanvasBgImage})` }}
-      >
-        <KanbanColumn tasks={todoTasks} type='To do'/>
-        <KanbanColumn tasks={inProgressTasks} type='In progress'/>
-        <KanbanColumn tasks={doneTasks} type='Done'/>
+      <div className={`flex flex-grow box-border w-full relative`}>
+        {/* Renderiza o modal de criação se estiver habilitado */}
+        {showCreationModal && <CreationModal closeDispatch={handleCloseCreationModal} />}
         
-      </div>
+        {/* Container principal do Kanban */}
+        <div className={`
+          m-4 w-full
+          rounded-[1.875rem]
+          items-center
+          justify-center
+          flex flex-grow flex-row
+          ${showFixedMetric  ? 'lg:w-3/4' : '' }
+        `}
+          style={{ backgroundImage: `url(${kanbanCanvasBgImage})` }} // Define a imagem de fundo
+        >
+          <KanbanColumn handleNewCard={setShowCreationModal} tasks={todoTasks} type='To do' /> {/* Coluna de tarefas a serem feitas */}
+          <KanbanColumn handleNewCard={setShowCreationModal} tasks={inProgressTasks} type='In progress' /> {/* Coluna de tarefas em progresso */}
+          <KanbanColumn handleNewCard={setShowCreationModal} tasks={doneTasks} type='Done' /> {/* Coluna de tarefas concluídas */}
+        </div>
 
-      <div className={`
-        ${showFixedMetric  ? 'lg:w-1/4': 'hidden' }
-        m-4
-        lg:static
-        absolute
-        right-0`}
-      >
-         {showFixedMetric && <FixedMetrics closeDispatch={setShowFixedMetric} />} 
+        {/* Container para métricas fixas, visível somente se showFixedMetric for verdadeiro */}
+        <div className={`
+          ${showFixedMetric  ? 'lg:w-1/4' : 'hidden' }
+          m-4
+          lg:static
+          absolute
+          right-0
+        `}>
+          {showFixedMetric && <FixedMetrics closeDispatch={setShowFixedMetric} />}
+        </div>
+
+        {/* Botão para exibir métricas fixas quando ocultas */}
+        {!showFixedMetric && 
+          <button
+            className='m-4 absolute w-[45px] h-[45px] bg-[#94A3B8] rounded-full top-0 right-0'
+            onClick={() => setShowFixedMetric(true)}
+          >
+            <BsPinFill className='text-white m-auto' />
+          </button>
+        }
       </div>
-      {!showFixedMetric && 
-      <>
-        <button className='m-4 absolute w-[45px] h-[45px] bg-[#94A3B8] rounded-full top-0 right-0' onClick={() => setShowFixedMetric(true)}><BsPinFill className='text-white m-auto' /></button>
-      </>
-      }
-    </div>
     </>
   )
 }
