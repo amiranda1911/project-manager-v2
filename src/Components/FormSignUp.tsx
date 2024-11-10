97531896d4af3a1097302264ba2d161f840b4626
Thais Sanches <thaisres@live.com>
2024-11-10 02:37:26 -0300

    style: fix signUp page

--- src/Components/FormSignUp.tsx
+++ src/Components/FormSignUp.tsx
@@ -95,7 +95,7 @@ const FormSignUp: React.FC = () => {
     return (
     <>
 
-    <form onSubmit={handleSubmit} className="flex flex-col gap-[1.5rem] md:mt-[1.938rem]">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-[1.9rem] md:mt-[1.938rem]">
        
         <section className="md:flex md:flex-row md:gap-[1.125rem] md:max-w-[32.5rem]">
             <div className="flex flex-col w-[20.563rem] h-[3.75rem]">
@@ -113,10 +113,10 @@ const FormSignUp: React.FC = () => {
                 className="font-roboto font-normal text-14 text-[#00000080] p-[0.75rem]
                  w-auto h-[2.25rem] border border-[#0000001A] rounded-md focus:outline-none focus:ring-1 focus:ring-[#00000080]"                    
                 />
-                {err.firstName && <p style={{ color: 'red' }}>{err.firstName}</p>}
+                {err.firstName && <p className="font-roboto font-normal text-12 leading-4 text-[#BD2323] md:text-13">{err.firstName}</p>}
             </div>
 
-            <div className="flex flex-col mt-[1.5rem] w-[20.563rem] h-[3.75rem]">
+            <div className="flex flex-col mt-[1.5rem] w-[20.563rem] h-[3.75rem] md:mt-0">
                 <label htmlFor="lastName" className="font-roboto font-medium text-left text-14 text-[#331436]">Last Name</label>                  
                 <input
                 type="text"
@@ -131,7 +131,7 @@ const FormSignUp: React.FC = () => {
                 className="font-roboto font-normal text-14 text-[#00000080] p-[0.75rem]
                 w-auto h-[2.25rem] border border-[#0000001A] rounded-md focus:outline-none focus:ring-1 focus:ring-[#00000080]"
                 />
-                {err.lastName && <p style={{ color: 'red' }}>{err.lastName}</p>}
+                {err.lastName && <p className="font-roboto font-normal text-12 leading-4 text-[#BD2323] md:text-13">{err.lastName}</p>}
             </div>
         </section>
        
@@ -150,7 +150,7 @@ const FormSignUp: React.FC = () => {
             className="font-roboto font-normal text-14 text-[#00000080] p-[0.75rem]
             w-auto h-[2.25rem] border border-[#0000001A] rounded-md focus:outline-none focus:ring-1 focus:ring-[#00000080] md:w-[32.5rem]"
             />
-            {err.email && <p style={{ color: 'red' }}>{err.email}</p>}
+            {err.email && <p className="font-roboto font-normal text-12 leading-4 text-[#BD2323] md:text-13 md:w-[32.5rem]">{err.email}</p>}
         </div>
 
         <div className="flex flex-col w-[20.563rem] h-[3.75rem]">
@@ -168,7 +168,7 @@ const FormSignUp: React.FC = () => {
             className="font-roboto font-normal text-14 text-[#00000080] p-[0.75rem]
             w-auto h-[2.25rem] truncate border border-[#0000001A] rounded-md focus:outline-none focus:ring-1 focus:ring-[#00000080] md:w-[32.5rem]"
             />
-            {err.jobPosition && <p style={{ color: 'red' }}>{err.jobPosition}</p>}
+            {err.jobPosition && <p className="font-roboto font-normal text-12 leading-4 text-[#BD2323] md:text-13 md:w-[32.5rem]">{err.jobPosition}</p>}
         </div>
 
         <div className="flex flex-col w-[20.563rem] h-[3.75rem]">
@@ -186,14 +186,14 @@ const FormSignUp: React.FC = () => {
             className="font-roboto font-normal text-14 text-[#00000080] p-[0.75rem]
             w-auto h-[2.25rem] border border-[#0000001A] rounded-md focus:outline-none focus:ring-1 focus:ring-[#00000080] md:w-[32.5rem]"
             />
-            {err.password && <p style={{ color: 'red' }}>{err.password}</p>}
+            {err.password && <p className="font-roboto font-normal text-12 leading-4 text-[#BD2323] md:text-13 md:w-[32.5rem]">{err.password}</p>}
         </div>
 
         {/* Exibe a mensagem de sucesso ou erro */}
         {err.formStatus && (
-            <p style={{ color: err.formStatus === errorMessages.formSuccess ? 'green' : 'red' }}>
+            <p style={{ color: err.formStatus === errorMessages.formSuccess ? '[#22C55E]' : '[#BD2323]' }}>
             {err.formStatus}
-            </p>
+            </p>  
         )}
 
         <div className=" mt-[1.413rem] mb-[1.313rem] md:mt-[2rem] md:mb-[1.938rem]">
