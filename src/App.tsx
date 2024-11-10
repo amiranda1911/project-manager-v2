fef36e8ef349a45f6e808a11865d3bcf5aaf16f8
Sabrina Souza <sabrinasouza.dev@gmail.com>
2024-11-10 15:57:38 -0300

    feature: Add message-toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import Routers from './routers';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
-  return <Routers />;
+  return (
+    <div>
+      <ToastContainer autoClose={3500}/>
+      <Routers />
+    </div>
+  );
 }
 
 export default App;
