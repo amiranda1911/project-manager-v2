1802f2bf85cee563a22bc5eb5feb7306ab406e25
AdsonTanajura <adson.tanajura@gmail.com>
2024-11-11 17:30:31 -0300

    Fix dinamica

--- src/pages/ProfilePage/LastesActivitySection/LastesActivityList/index.tsx
+++ src/pages/ProfilePage/LastesActivitySection/LastesActivityList/index.tsx
@@ -1,16 +1,18 @@
 import LatestActivityItem from './Item/Item';
+interface LastesActivityListProps {
+  avata: string;
+}
 
-const LastesActivityList = () => {
+const LastesActivityList = ({ avata }: LastesActivityListProps) => {
   return (
     <div className="my-16 max-h-[34.0625rem] h-[34.0625rem] ">
       <ul className="flex gap-8 flex-col max-h-[41.625rem]">
-        <LatestActivityItem />
-        <LatestActivityItem />
-        <LatestActivityItem />
-        <LatestActivityItem />
-        <LatestActivityItem />
-        <LatestActivityItem />
-        <LatestActivityItem />
+        <LatestActivityItem avata={avata} />
+        <LatestActivityItem avata={avata} />
+        <LatestActivityItem avata={avata} />
+        <LatestActivityItem avata={avata} />
+        <LatestActivityItem avata={avata} />
+        <LatestActivityItem avata={avata} />
       </ul>
     </div>
   );
