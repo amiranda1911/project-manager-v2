1bbd9b1fa90c8adf26f802abc2011a2aafbce7ea
AdsonTanajura <adson.tanajura@gmail.com>
2024-11-11 15:00:33 -0300

    Fix: Redirects all pages

--- src/pages/BlockedPage/BlockedPage.tsx
+++ src/pages/BlockedPage/BlockedPage.tsx
@@ -1,44 +1,52 @@
-import MainHeader from "../../Components/MainHeader"
-import MainFooter from "../../Components/MainFooter"
+import MainHeader from '../../Components/MainHeader';
+import MainFooter from '../../Components/MainFooter';
+import { Link } from 'react-router-dom';
 
 const BlockedPage = () => {
   return (
     <>
-    <header>
-        <MainHeader pageType='page2'/>
-    </header>
+      <header>
+        <MainHeader pageType="page2" />
+      </header>
 
-    <main className="flex flex-col h-screen items-center justify-center bg-white 
-            md:flex-row md:w-[43.75rem] md:mx-auto lg:w-[59.75rem]">
-      
+      <main
+        className="flex flex-col h-screen items-center justify-center bg-white 
+            md:flex-row md:w-[43.75rem] md:mx-auto lg:w-[59.75rem]"
+      >
         <div className="flex flex-col h-full justify-center flex-grow md:flex-row md:w-[43.938rem] md:mx-auto">
-          
-            <div className="flex items-center justify-center md:place-content-start">
-                <img 
-                src="src\assets\Lock403.svg" 
-                alt="403 Not Found" 
-                className="w-[12.68rem] md:w-[22.912rem] lg:w-[28.438rem]"
-                />
-            </div>
-                      
-            <div className="flex flex-col items-center text-center w-[14.375rem] mx-auto md:place-content-center md:w-[20.22rem]  ">
-                <h1 className="font-roboto font-normal text-26 md:text-[6rem] md:w-[20.22rem] md:text-left lg:w-[27.5rem]">403</h1>
-                <p className="font-roboto font-normal text-black text-[1.125rem] leading-[2] md:text-19 md:text-left lg:w-[27.5rem]">
-                    Nothing to see here... yet! To see this content, you need to 
-                    <span> 
-                        <a href="/LoginPage" className="text-[#4f46e5] underline font-roboto font-bold text-15 hover:text-[#268FE4] md:text-19"> log in</a>
-                    </span>
-                </p>                      
-            </div>
-            
-        </div>
-    </main>
+          <div className="flex items-center justify-center md:place-content-start">
+            <img
+              src="src\assets\Lock403.svg"
+              alt="403 Not Found"
+              className="w-[12.68rem] md:w-[22.912rem] lg:w-[28.438rem]"
+            />
+          </div>
 
-    <footer>
+          <div className="flex flex-col items-center text-center w-[14.375rem] mx-auto md:place-content-center md:w-[20.22rem]  ">
+            <h1 className="font-roboto font-normal text-26 md:text-[6rem] md:w-[20.22rem] md:text-left lg:w-[27.5rem]">
+              403
+            </h1>
+            <p className="font-roboto font-normal text-black text-[1.125rem] leading-[2] md:text-19 md:text-left lg:w-[27.5rem]">
+              Nothing to see here... yet! To see this content, you need to
+              <span>
+                <Link
+                  to="/LoginPage"
+                  className="text-[#4f46e5] underline font-roboto font-bold text-15 hover:text-[#268FE4] md:text-19"
+                >
+                  {' '}
+                  log in
+                </Link>
+              </span>
+            </p>
+          </div>
+        </div>
+      </main>
+
+      <footer>
         <MainFooter />
-    </footer>
+      </footer>
     </>
   );
-}
+};
 
-export default BlockedPage
+export default BlockedPage;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,25 @@
-import HomePage from "../../Components/HomePage"
+import { useNavigate } from 'react-router-dom';
+import HomePage from '../../Components/HomePage';
+import { useAuth } from '../../hooks/useAuth';
+import { useEffect } from 'react';
 
 const Home = () => {
+  const { verifyIsAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const isAuthenticated = verifyIsAuthenticated();
+    console.log(isAuthenticated);
+    if (isAuthenticated) {
+      navigate('/kanban');
+    }
+  }, [verifyIsAuthenticated, navigate]);
   return (
     <div>
       {/* <h1 className="text-5xl font-bold">Project Manager</h1> */}
-      <HomePage/>
+      <HomePage />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
